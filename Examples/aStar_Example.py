74d9ce0a0b974b6d56bd4013d296575f0accc4f0
Jonathan Isely <63868039+JonathanI19@users.noreply.github.com>
2022-10-16 21:35:07 -0400

    Update aStar_Example.py
    Adding comments

--- Examples/aStar_Example.py
+++ Examples/aStar_Example.py
@@ -2,20 +2,38 @@
 
 from pyamaze import maze
 import sys
+
+# Importing aStar method and ParseFile object
 sys.path.append("../")
 from Algorithms.aStar import aStar
 from parser_script import ParseFile
 
+# Names for Output csv and Input csv
 OUTPUT = "./aStar_example_output.csv"
 INPUT = "./example_input_2.txt"
 
+# Main function
 def main():
+    
+    # Creating parse object.
+    # Input csv is out of order, so N, S, E, W is used to specify the order that they are passed in.
+    # Output csv will be normalized to EWNS, provided proper input order is declared.
     parse = ParseFile(input_file = INPUT, output_file = OUTPUT, N=0, S=1, E=2, W=3)
+    
+    # creating maze object of name m
     m = maze(rows = parse.rows, cols = parse.cols)
+    
+    # Loading maze based on normalized output csv
+    # set x and y variables in CreateMaze to change goal; Default is 1,1
     m.CreateMaze(loadMaze = OUTPUT)
+    
+    # Using aStar to create fwdPath dictionary, which is used to find path from start to goal.
     fwdPath= aStar(m = m, start = None)
+    
+    # Print out fwdPath dictionary
     print(fwdPath)
-
+    
+    # If user passes in True as an argument after calling this script, a GUI will display the maze
     if len(sys.argv) > 1:
         if sys.argv[1] == "True":    
             m.run()
